23916192bfbe6c3a0309ddb58059dffe517772d9
Acer <d.m.j-99@abv.bg>
2022-12-28 15:57:58 +0200

    commit

--- StoreWebSystem/Controllers/ProductsController.cs
+++ StoreWebSystem/Controllers/ProductsController.cs
@@ -1,6 +1,153 @@
-﻿namespace StoreWebSystem.Controllers
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreWebSystem.Models;
+using StoreWebSystem.Services;
+using System.Threading.Tasks;
+
+namespace StoreWebSystem.Controllers
 {
-    public class ProductsController
+    public class ProductsController : Controller
     {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public ProductsController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _productService.GetByIdAsync(id.Value);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewData["CategoryId"] = new SelectList(await _categoryService.GetAllAsync(), "Id", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductDto product)
+        {
+            if (ModelState.IsValid)
+            {
+                var uplaodResult = await _productService.UploadFileToFileSystem(product.ImageFile);
+                product.ImagePath = uplaodResult.DatabaseValue;
+                await _productService.CreateAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(await _categoryService.GetAllAsync(),
+                "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _productService.GetByIdAsync(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(
+                await _categoryService.GetAllAsync(),
+                "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductDto product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _productService.UpdateAsync(id, product);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await _productService.ExistAsync(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(
+                await _categoryService.GetAllAsync(),
+                "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _productService.GetByIdAsync(id.Value);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _productService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
